74922a70e3bf130a865c76a4cc44aa8c02d21c23
thucpn <thucsh2@gmail.com>
2021-11-14 13:01:08 +0700

    feat: init ext

--- Frontend Extension/background.js
+++ Frontend Extension/background.js
@@ -1,6 +1,6 @@
-let color = '#3aa757';
+let autoCheck = false;
+let currentUser = null;
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+chrome.runtime.onInstalled.addListener(async () => {
+	await chrome.storage.sync.set({ autoCheck, currentUser });
 });
