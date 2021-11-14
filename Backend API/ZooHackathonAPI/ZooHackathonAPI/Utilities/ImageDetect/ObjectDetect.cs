9bca0e7edaab81d5f71eb4c3182c6894bc571e61
Nguyen Lam Nhat Tien <tien20001810@gmail.com>
2021-11-14 21:26:40 +0700

    Init and finish methods for ImageDetector

---
+++ Backend API/ZooHackathonAPI/ZooHackathonAPI/Utilities/ImageDetect/ObjectDetect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZooHackathonAPI.Utilities.ImageDetect
+{
+    public class ObjectDetect
+    {
+        public float Probability { get; set; }
+        public string Label { get; set; }
+    }
+}
