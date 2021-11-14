3320aa59112bf6512ced11c89b98f45807c09c42
Anh-Dung-IT <anhdungit.ni@gmail.com>
2021-11-14 07:36:38 +0700

    create structure Controller folder, Models folder

---
+++ Backend API/ZooHackathonAPI/ZooHackathonAPI/Models/User/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZooHackathonAPI.Models.User
+{
+    public class UserDTO
+    {
+    }
+}
